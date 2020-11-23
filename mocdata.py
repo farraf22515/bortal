fb566f09d82df70324080e54d55b689a07a28620
Pichet Boonpheng <pichet.boonpheng@gmail.com>
2020-11-23 23:23:21 +0700

    1

--- mocdata.py
+++ mocdata.py
@@ -1,4 +1,5 @@
 import random
 
 for i in range(30):
-    an_id = str(random.randint(100, 999))
+    ann_id = str(random.randint(100, 999))
+    ann_text = "Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast yardarm. Pinnace holystone mizzenmast quarter crow's nest nipperkin grog yardarm hempen halter furl. Swab barque interloper chantey doubloon starboard grog black jack gangway rutters."
